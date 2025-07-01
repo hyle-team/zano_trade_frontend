50c2c3f8e1ce5b42eabae4630e62a76351e2463d
jejolare <jejolare@gmail.com>
2025-07-01 19:16:25 +0700

    debug

--- src/store/store-reducer.tsx
+++ src/store/store-reducer.tsx
@@ -14,25 +14,25 @@ const initialState: ContextState = {
 
 const reducer = (state: ContextState, action: ContextAction): ContextState => {
 	switch (action.type) {
-	case 'WALLET_UPDATED':
-		return { ...state, wallet: action.payload };
+		case 'WALLET_UPDATED':
+			return { ...state, wallet: action.payload };
 
-	case 'USER_UPDATED':
-		return { ...state, user: action.payload };
+		case 'USER_UPDATED':
+			return { ...state, user: action.payload };
 
-	case 'CONFIG_UPDATED':
-		return { ...state, config: action.payload };
+		case 'CONFIG_UPDATED':
+			return { ...state, config: action.payload };
 
-	case 'TRIGGER_OFFERS_UPDATE':
-		return { ...state, __triggers: { ...state.__triggers, offers: Math.random() } };
-	case 'ASSETS_RATE_UPDATED': {
-		return { ...state, assetsRates: new Map([...state.assetsRates, ...action.payload]) };
-	}
-	case 'CLOSED_NOTIFICATIONS_UPDATED': {
-		return { ...state, closed_notifications: action.payload };
-	}
-	default:
-		return { ...state };
+		case 'TRIGGER_OFFERS_UPDATE':
+			return { ...state, __triggers: { ...state.__triggers, offers: Math.random() } };
+		case 'ASSETS_RATE_UPDATED': {
+			return { ...state, assetsRates: new Map([...state.assetsRates, ...action.payload]) };
+		}
+		case 'CLOSED_NOTIFICATIONS_UPDATED': {
+			return { ...state, closed_notifications: action.payload };
+		}
+		default:
+			return { ...state };
 	}
 };
 
