c8b5f665749ba61fa9b13e57cc73f0f86bfd9399
AzizbekFayziyev <fayziyev.dev@gmail.com>
2025-07-04 19:08:52 +0500

    fix: socket

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,10 @@
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-const socket = io();
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+const socket = io(SOCKET_URL, {
+	transports: ['websocket'],
+	withCredentials: true,
+});
 
 export default socket;
