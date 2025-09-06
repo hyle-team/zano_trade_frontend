cbde610e47199f5a877a5228a1c7800d4619df1c
AzizbekFayziyev <fayziyev.dev@gmail.com>
2025-09-06 14:27:53 +0500

    add: interactive guide

--- src/components/dex/TradingHeader/index.tsx
+++ src/components/dex/TradingHeader/index.tsx
@@ -6,11 +6,13 @@ import BackButton from '@/components/default/BackButton/BackButton';
 import { roundTo, notationToString, classes } from '@/utils/utils';
 import questionIcon from '@/assets/images/UI/question.svg';
 import Image from 'next/image';
+import { useTour } from '@/store/guide-provider';
 import styles from './styles.module.scss';
 import StatItem from './components/StatItem';
 import { TradingHeaderProps } from './types';
 import CurrencyIcon from './components/CurrencyIcon';
 import AssetRow from './components/AssetRow';
+import DexGuide from './components/DexGuide';
 
 const TradingHeader = ({
 	pairStats,
@@ -21,6 +23,8 @@ const TradingHeader = ({
 	secondAssetId,
 	pairData,
 }: TradingHeaderProps) => {
+	const { start, isRunning } = useTour('dex-onboarding');
+
 	const currencyNames = {
 		firstCurrencyName: pairData?.first_currency?.name || '',
 		secondCurrencyName: pairData?.second_currency?.name || '',
@@ -59,76 +63,85 @@ const TradingHeader = ({
 	];
 
 	return (
-		<div className={styles.header}>
-			<div className={styles.header__stats}>
-				<div className={styles.header__currency}>
-					<div className={styles.header__currency_icon}>
-						<CurrencyIcon code={firstAssetId} />
-					</div>
+		<>
+			<DexGuide />
+			<div className={styles.header}>
+				<div className={styles.header__stats}>
+					<div className={styles.header__currency}>
+						<div className={styles.header__currency_icon}>
+							<CurrencyIcon code={firstAssetId} />
+						</div>
 
-					<div className={styles.header__currency_item}>
-						<p className={styles.currencyName}>
-							{!pairData ? (
-								'...'
-							) : (
-								<>
-									{firstCurrencyName}
-									<span>/{secondCurrencyName}</span>
-								</>
-							)}
-						</p>
-
-						<div className={styles.price}>
-							<p
-								className={classes(
-									styles.price__secondCurrency,
-									coefficientOutput >= 0 ? styles.green : styles.red,
+						<div className={styles.header__currency_item}>
+							<p className={styles.currencyName}>
+								{!pairData ? (
+									'...'
+								) : (
+									<>
+										{firstCurrencyName}
+										<span>/{secondCurrencyName}</span>
+									</>
 								)}
-							>
-								{roundTo(notationToString(pairStats?.rate || 0, 8))}
 							</p>
-							{pairRateUsd && <p className={styles.price__usd}>~ ${pairRateUsd}</p>}
+
+							<div className={styles.price}>
+								<p
+									className={classes(
+										styles.price__secondCurrency,
+										coefficientOutput >= 0 ? styles.green : styles.red,
+									)}
+								>
+									{roundTo(notationToString(pairStats?.rate || 0, 8))}
+								</p>
+								{pairRateUsd && (
+									<p className={styles.price__usd}>~ ${pairRateUsd}</p>
+								)}
+							</div>
 						</div>
 					</div>
+
+					{pairData && firstAssetLink && secondAssetLink && (
+						<div className={styles.header__stats_assets}>
+							<AssetRow
+								name={firstCurrencyName}
+								link={firstAssetLink}
+								id={firstAssetId || ''}
+								code={firstAssetId}
+							/>
+							<AssetRow
+								name={secondCurrencyName}
+								link={secondAssetLink}
+								id={secondAssetId || ''}
+								code={secondAssetId}
+							/>
+						</div>
+					)}
+
+					{stats.map(({ Img, title, value, coefficient }) => (
+						<StatItem
+							key={title}
+							className={styles.header__stats_item}
+							Img={Img}
+							title={title}
+							value={value}
+							coefficient={coefficient}
+						/>
+					))}
 				</div>
 
-				{pairData && firstAssetLink && secondAssetLink && (
-					<div className={styles.header__stats_assets}>
-						<AssetRow
-							name={firstCurrencyName}
-							link={firstAssetLink}
-							id={firstAssetId || ''}
-							code={firstAssetId}
-						/>
-						<AssetRow
-							name={secondCurrencyName}
-							link={secondAssetLink}
-							id={secondAssetId || ''}
-							code={secondAssetId}
-						/>
-					</div>
-				)}
+				<div className={styles.header__actions}>
+					<button
+						onClick={start}
+						disabled={isRunning}
+						className={styles.header__actions_guide}
+					>
+						<Image src={questionIcon} width={24} height={24} alt="guide" />
+					</button>
 
-				{stats.map(({ Img, title, value, coefficient }) => (
-					<StatItem
-						key={title}
-						className={styles.header__stats_item}
-						Img={Img}
-						title={title}
-						value={value}
-						coefficient={coefficient}
-					/>
-				))}
+					<BackButton className={styles.header__actions_backBtn} isSm />
+				</div>
 			</div>
-
-			<div className={styles.header__actions}>
-				<button className={styles.header__actions_guide}>
-					<Image src={questionIcon} width={24} height={24} alt="guide" />
-				</button>
-
-				<BackButton className={styles.header__actions_backBtn} isSm />
-			</div>
-		</div>
+		</>
 	);
 };
 
