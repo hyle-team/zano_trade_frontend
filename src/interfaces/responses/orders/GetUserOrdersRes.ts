9cb526357d925fc6d63eac71e738f612b58df2b7
Andrew Besedin <andrewprog97@gmail.com>
2026-02-20 13:29:48 +0300

    update: fix prices in My Orders page

--- src/interfaces/responses/orders/GetUserOrdersRes.ts
+++ src/interfaces/responses/orders/GetUserOrdersRes.ts
@@ -1,7 +1,28 @@
 import CurrencyRow from '@/interfaces/common/CurrencyRow';
 import OfferType from '@/interfaces/common/OfferType';
 
-interface UserOrderData {
+export type GetUserOrdersResCurrency = {
+	id: number;
+	name: string;
+	code: string;
+	type: string;
+	asset_id: string;
+	auto_parsed: boolean;
+	asset_info?: {
+		asset_id: string;
+		logo: string;
+		price_url: string;
+		ticker: string;
+		full_name: string;
+		total_max_supply: string;
+		current_supply: string;
+		decimal_point: number;
+		meta_info: string;
+	};
+	whitelisted: boolean;
+};
+
+export interface UserOrderData {
 	id: string;
 	type: OfferType;
 	timestamp: string;
@@ -15,6 +36,21 @@ interface UserOrderData {
 	left: number;
 	first_currency: CurrencyRow;
 	second_currency: CurrencyRow;
+
+	pair: {
+		id: number;
+		first_currency_id: number;
+		second_currency_id: number;
+		rate?: number;
+		coefficient?: number;
+		high?: number;
+		low?: number;
+		volume: number;
+		featured: boolean;
+
+		first_currency: GetUserOrdersResCurrency;
+		second_currency: GetUserOrdersResCurrency;
+	};
 }
 
 interface GetUserOrdersRes {
@@ -24,5 +60,3 @@ interface GetUserOrdersRes {
 }
 
 export default GetUserOrdersRes;
-
-export type { UserOrderData };
