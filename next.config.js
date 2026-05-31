45eb7b0c22d3546fb88257e3ce9086e3d12f972c
AzizbekFayziyev <fayziyevazizbek07@gmail.com>
2026-05-31 23:23:40 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -24,12 +24,12 @@ const nextConfig = {
 						key: 'Content-Security-Policy',
 						value: [
 							"default-src 'self'",
-							"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com",
+							"script-src 'self' 'unsafe-inline' https://www.googletagmanager.com",
 							"style-src 'self' 'unsafe-inline'",
-							"img-src 'self' data: blob: https:",
-							"font-src 'self' data:",
-							"connect-src 'self' https: wss: ws:",
-							"media-src 'self' blob:",
+							"img-src 'self' data: blob:",
+							"font-src 'self'",
+							"connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com",
+							"media-src 'self'",
 							"object-src 'none'",
 							"frame-ancestors 'none'",
 							"base-uri 'self'",
@@ -37,41 +37,23 @@ const nextConfig = {
 							'upgrade-insecure-requests',
 						].join('; '),
 					},
-
 					{
 						key: 'Strict-Transport-Security',
 						value: 'max-age=63072000; includeSubDomains; preload',
 					},
-
-					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff',
-					},
-
-					{
-						key: 'X-Frame-Options',
-						value: 'SAMEORIGIN',
-					},
-
-					{
-						key: 'X-XSS-Protection',
-						value: '1; mode=block',
-					},
-
-					{
-						key: 'Referrer-Policy',
-						value: 'same-origin',
-					},
-
+					{ key: 'X-Content-Type-Options', value: 'nosniff' },
+					{ key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+					{ key: 'X-XSS-Protection', value: '1; mode=block' },
+					{ key: 'Referrer-Policy', value: 'same-origin' },
 					{
 						key: 'Permissions-Policy',
 						value: [
-							'camera=(self)',
-							'microphone=(self)',
-							'display-capture=(self)',
-							'fullscreen=(self)',
-							'picture-in-picture=(self)',
-							'speaker-selection=(self)',
+							'camera=()',
+							'microphone=()',
+							'display-capture=()',
+							'fullscreen=()',
+							'picture-in-picture=()',
+							'speaker-selection=()',
 							'geolocation=()',
 							'payment=()',
 							'usb=()',
