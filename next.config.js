e6831bb94bc72ccf7b2ad8b8fdd8b224e23484f6
AzizbekFayziyev <fayziyevazizbek07@gmail.com>
2026-05-31 23:18:49 +0500

    feat: add common security headers and HSTS

--- next.config.js
+++ next.config.js
@@ -15,6 +15,72 @@ const nextConfig = {
 			// },
 		];
 	},
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: [
+							"default-src 'self'",
+							"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com",
+							"style-src 'self' 'unsafe-inline'",
+							"img-src 'self' data: blob: https:",
+							"font-src 'self' data:",
+							"connect-src 'self' https: wss: ws:",
+							"media-src 'self' blob:",
+							"object-src 'none'",
+							"frame-ancestors 'none'",
+							"base-uri 'self'",
+							"form-action 'self'",
+							'upgrade-insecure-requests',
+						].join('; '),
+					},
+
+					{
+						key: 'Strict-Transport-Security',
+						value: 'max-age=63072000; includeSubDomains; preload',
+					},
+
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN',
+					},
+
+					{
+						key: 'X-XSS-Protection',
+						value: '1; mode=block',
+					},
+
+					{
+						key: 'Referrer-Policy',
+						value: 'same-origin',
+					},
+
+					{
+						key: 'Permissions-Policy',
+						value: [
+							'camera=(self)',
+							'microphone=(self)',
+							'display-capture=(self)',
+							'fullscreen=(self)',
+							'picture-in-picture=(self)',
+							'speaker-selection=(self)',
+							'geolocation=()',
+							'payment=()',
+							'usb=()',
+						].join(', '),
+					},
+				],
+			},
+		];
+	},
 	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/i,
